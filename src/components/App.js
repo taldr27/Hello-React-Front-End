f7d0ed85221df34a611a6d9bda8bdc16c4efcc3c
lana <diegogarciaq98@gmail.com>
2023-02-28 00:18:12 -0500

    Generate the front end app

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
